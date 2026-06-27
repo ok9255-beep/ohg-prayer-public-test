2e4af0fceeb033f85c7ea6ae6fc8f7e439e8e62c
ok9255-beep <ok9255@gmail.com>
2026-06-27 19:26:44 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste your deployed Apps Script Web App URL here.
 // Example: https://script.google.com/macros/s/AKfycb.../exec
-window.OHG_API_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+window.OHG_API_URL = 'https://script.google.com/macros/s/AKfycbz5US5Qgkm8WiaRNuT2taabclo2-sf8GZmSK3Vf-dnv_5mnCQzlyJN2iV33kM4xmSsN/exec';
